70898cf665029298b6f21ff41da6223fe91eeb5d
Vasily Medyanikov <vasily.medyanikov@oculus.com>
2016-07-11 16:13:39 +0100

    git stuff

--- scripts/bisect.py
+++ scripts/bisect.py
@@ -13,6 +13,8 @@ print "Running pre-script"
 if args.pre:
     process = subprocess.Popen(args.pre, stdout=subprocess.PIPE, shell=True)
     print process.communicate()[0]
+    if process.returncode:
+        sys.exit(128)
 
 while True:
     t = raw_input("Is revision good? (y/n)")
@@ -22,10 +24,15 @@ while True:
     if t.capitalize() == "N":
         revision_is_bad = True
         break
+    if t.capitalize() == "A":
+        print "Abort"
+        sys.exit(128)
 
 print "Running post-script"
 if args.post:
     process = subprocess.Popen(args.post, stdout=subprocess.PIPE, shell=True)
     print process.communicate()[0]
+    if process.returncode:
+        sys.exit(128)
 
 sys.exit(revision_is_bad)
